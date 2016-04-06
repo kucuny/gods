dfe1551b884dc5641792bc625d79afa32c4be24f
kucuny <kucuny@gmail.com>
2016-04-06 10:22:42 +0900

    Add struct push/pop test case in queue

--- queue/queue_test.go
+++ queue/queue_test.go
@@ -10,6 +10,11 @@ type QueueTestSuite struct {
 	q *Queue
 }
 
+type testNode struct {
+	strValue string
+	intValue int
+}
+
 func (suite *QueueTestSuite) SetupSuite() {
 	suite.q = NewQueue()
 }
@@ -21,16 +26,20 @@ func (suite *QueueTestSuite) TestQueue() {
 	suite.q.Push("1235")
 	suite.q.Push(1234)
 	suite.q.Push(345.123)
+	suite.q.Push(testNode{strValue: "test1", intValue: 30})
 
-	suite.Equal(4, suite.q.Len())
+	suite.Equal(5, suite.q.Len())
 	suite.Equal("abcd", suite.q.Pop())
-	suite.Equal(3, suite.q.Len())
+	suite.Equal(4, suite.q.Len())
 	suite.Equal("1235", suite.q.Pop())
-	suite.Equal(2, suite.q.Len())
+	suite.Equal(3, suite.q.Len())
 	suite.Equal(1234, suite.q.Pop())
-	suite.Equal(1, suite.q.Len())
+	suite.Equal(2, suite.q.Len())
 	suite.Equal(345.123, suite.q.Peek())
+	suite.Equal(2, suite.q.Len())
+	suite.Equal(345.123, suite.q.Pop())
 	suite.Equal(1, suite.q.Len())
+	suite.Equal(testNode{strValue: "test1", intValue: 30}, suite.q.Pop().(testNode))
 }
 
 func TestQueueTestSuite(t *testing.T) {
